3af492529670ede96aef2bb4c82304ef9b056561
zichguan-amd <zichuan.guan@amd.com>
2026-03-20 16:49:11 -0400

    Add torch skip tests
    Signed-off-by: zichguan-amd <zichuan.guan@amd.com>

--- external-builds/pytorch/skip_tests/generic.py
+++ external-builds/pytorch/skip_tests/generic.py
@@ -243,6 +243,7 @@ skip_tests = {
             # The callstack for this one points to _fill_mem_eff_dropout_mask, so it may be related to aotriton?
             "test_cublas_config_nondeterministic_alert_cuda",
             # Large test that isn't very CI-friendly (takes ~2 seconds, possibly hanging)
+            "test_int64_upsample3d_cuda_bfloat16",
             "test_memory_format_operators_cuda",
             # Flaky tests hanging on some gfx1151 machines...
             # Maybe memory pressure? Tests use some large tensors:
